e447551a287e67ef07b0b1d872788db107234f1f
HackTheOxidation <tomas.hagenau@protonmail.ch>
2022-12-20 13:49:54 +0100

    Began working on a new site in Svelte.

---
+++ codeberg-pages/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import NavBar from './lib/NavBar.svelte'
+  import Greeting from './lib/Greeting.svelte'
+</script>
+
+<main>
+  <NavBar/>
+  <Greeting/>
+</main>
+
+<style>
+</style>
---
+++ codeberg-pages/src/lib/Greeting.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<div class="greetings-container">
+  <p>Greetings, Traveller!</p>
+  <p>
+    Welcome to HackTheOxidation's Codeberg pages, a peaceful place in the vastness of cyberspace.
+  </p>
+  <p>
+    I'm a software engineering student and free/open-source software enthusiast among other things.
+  </p>
+  <p>
+    This site is still under construction at the moment (seriously, when are you ever done with a software project?).<br>
+    In the mean time, check out my projects:
+  </p>
+  <p>
+    <a href="https://codeberg.org/HackTheOxidation">My Git repository on codeberg.org</a>
+  </p>
+</div>
+
+<style>
+  .greetings-container {
+    border: 2px solid #bbb;
+    border-radius: 5px;
+  }
+
+  p {
+    margin: 3px;
+    margin-left: 8px;
+    margin-right: 8px;
+  }
+</style>
